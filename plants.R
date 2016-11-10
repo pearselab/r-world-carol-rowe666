4b84282f3feee7c315d2fc4410f2c774a67a87bc
Carol Rowe <carolrowe@Carols-MacBook-Air.local>
2016-11-10 10:15:22 -0700

    fubar

--- plants.R
+++ plants.R
@@ -1,21 +1,21 @@
- setup.plants <- function(repro, survive, comp.mat, names=NULL){
-   if(is.null(names)){
+setup.plants <- function(repro, survive, comp.mat, names=NULL){
+  if(is.null(names)){
     names <- letters[seq_along(repro)]
    }
-   if(length(names) < length(repro)){
-     stop("You didn't enter the correct number of species")
+  if(length(names) < length(repro)){
+    stop("You didn't enter the correct number of species")
    }
   if(length(repro) != length(survive)){
     stop("Reproduction and survival parameters needed for all species")
   }
-   if(any(repro > 1) | any(repro < 0)){
-     stop("Probabilities out of range in repro vector")
+  if(any(repro > 1) | any(repro < 0)){
+    stop("Probabilities out of range in repro vector")
   }
-   if(any(repro > 1) | any(repro < 0)){
-     stop("Probabilities out of range in survive vector")
+  if(any(repro > 1) | any(repro < 0)){
+    stop("Probabilities out of range in survive vector")
    }
-   if(any(repro > 1) | any(repro < 0)){
-     stop("Probabilities out of range in competition matrix")
+  if(any(repro > 1) | any(repro < 0)){
+    stop("Probabilities out of range in competition matrix")
    }
   repro <- setNames(repro, names)
   survive <- setNames(survive, names)
@@ -27,13 +27,14 @@
  
 repro <- runif(n=4, min=0, max=1)
 print(repro)
-survive <- runif(n=length(repro), min=0, max=1)
+survive <- c(0.1, 0.3, 0.6, 0.8)
 comps <- runif(n=length(repro), min=0, max=1)
 comps <- runif(n=length(repro)^2, min = 0, max = 1)
 comp.mat <- matrix(comps, nrow = length(repro), ncol = length(repro))
-names <- list('Betula', 'Popoulus', 'Pteridium', 'Pseudostuga')
+names <- list('Betula', 'Popoulus', 'Pteridium', 'Pseudotsuga')
 
 # Not sure if I'm supposed to reassign.
+# Can anyone find a good tutorial on setNames? Know what it's doing, but not how to utilize the output.
 info <- setup.plants(repro, survive, comp.mat, names)
 names <- info$names
 repro <- info$repro
@@ -41,21 +42,25 @@ survive <- info$survive
 comp.mat <- info$comp.mat
 
 
-cat("testing to see what's going on:", survive, "\n")
 
-#Tested this function by substituting "Betula" for plants. It works.
+#Tested this function by substituting "Betula" for names. It seems to work.
+# survive: if cell is NA or less than 0, then leave as is.
+# if survives, then give cell the plant name
+# if doesn't survive, then make the cell empty
 survive <- function(cell, info){
-  if(cell == "NA" | cell == ''){
-    return(cell)
+  if(is.na(cell) | cell < 0){
+    cell <- cell
   }
-  names <- "Betula"
   if(runif(1) <= info$survive[names]){
-    # [[plant]] if you want just the numeric value without the name
-    cell <- info$survive[names]
-    cat("my cell is:", cell, "\n")
-    return(cell)
+    names <- info$survive[names]
+    # [[names]] if you want just the numeric value without the name
+    cell <- names
+  }else{
+    cell <- ""
   }
+return(cell)
 }
+
 
 # terrain is matrix I have called my.world
 plant.timestep <- function(my.world, info){
@@ -70,7 +75,11 @@ plant.timestep <- function(my.world, info){
   return(new.plants.matrix)
 }
 
+
+
+
 ## creating a dummy matrix for my.world
+## woould be nice to test to see if function works
 data <- rnorm(10, 1, 20)
 my.world <- matrix(data =data, nrow = 5, ncol = 5)
 my.world
@@ -78,6 +87,7 @@ new.plants.matrix <- plant.timestep(my.world, info)
 new.plants.matrix
 
 # plants <- array("", dim=c(dim(terrain),timesteps+1))
-# #...why timesteps+1, do you think?...
+# #...why timesteps+1, do you think?... Because you are entering info into the next timestep!
 # for(i in seq_len(dim(plants)[3]))
 #   plants[,,i][is.na(terrain)] <- NA
+
