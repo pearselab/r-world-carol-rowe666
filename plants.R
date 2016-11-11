7eca480b993884e3cd6780f043e4dd0a4cb7d210
Carol Rowe <carolrowe@Carols-MacBook-Air.local>
2016-11-10 17:36:48 -0700

    Up to plants array

--- plants.R
+++ plants.R
@@ -1,3 +1,7 @@
+## Anyone looking at this.....does my logic make sense???
+## Have NOT worked on the plants array yet...that's where I left off.
+
+######## SETUP.PLANTS FUNCTION ################
 setup.plants <- function(repro, survive, comp.mat, names=NULL){
   if(is.null(names)){
     names <- letters[seq_along(repro)]
@@ -19,12 +23,13 @@ setup.plants <- function(repro, survive, comp.mat, names=NULL){
    }
   repro <- setNames(repro, names)
   survive <- setNames(survive, names)
-  #...what does the line above do? Do you want more like it?
   return(list(repro=repro, survive=survive, comp.mat=comp.mat,
               names=names))
 }
 
- 
+###### END OF SETUP.PLANTS FUNCTION ############
+
+## setting up some dummies to play with for testing
 repro <- runif(n=4, min=0, max=1)
 print(repro)
 survive <- c(0.1, 0.3, 0.6, 0.8)
@@ -36,58 +41,89 @@ names <- list('Betula', 'Popoulus', 'Pteridium', 'Pseudotsuga')
 # Not sure if I'm supposed to reassign.
 # Can anyone find a good tutorial on setNames? Know what it's doing, but not how to utilize the output.
 info <- setup.plants(repro, survive, comp.mat, names)
-names <- info$names
-repro <- info$repro
-survive <- info$survive
-comp.mat <- info$comp.mat
+# names <- info$names
+# repro <- info$repro
+# survive <- info$survive
+# comp.mat <- info$comp.mat
 
-
+# repro[1] # this gives me both Betula AND the repro number
+# repro[[1]] or repro["Betula"] # either of these gives me JUST the repro number
+### LOOKY HERE! TOOK ME FOREVER TO FIGURE THIS SIMPLE THING OUT TO RETURN THE NAME!
+# names(repro[1]) # AH HA!! this gives me JUST the name of the plant!!! This took LOTS of playing to figure out!!!
 
 #Tested this function by substituting "Betula" for names. It seems to work.
 # survive: if cell is NA or less than 0, then leave as is.
 # if survives, then give cell the plant name
 # if doesn't survive, then make the cell empty
+## And jsut WHAT is CELL???? is that [i,j] from what??? Or, am I putting something INTO the cell? THen why are
+## you calling cell in the function: function(cell, info), rather, I'd do function(info) and then return(cell)
+#### Will, you've got me VERY confused.
+## for now, I have cell going in and cell going out.....
+## Update on comments: I think that cell is from terrain (my.world) matrix to see if we can even put a plant there.
+## So, my thinking is that we return "cell" such that the new matrix created in plant.timestep will retain the NA, the '', or the plant name.
+
+#########   SURVIVE FUNCTION ###############
 survive <- function(cell, info){
   if(is.na(cell) | cell < 0){
     cell <- cell
   }
-  if(runif(1) <= info$survive[names]){
-    names <- info$survive[names]
-    # [[names]] if you want just the numeric value without the name
-    cell <- names
+  # this makes it easier for me
+  survive <- info$survive
+  # I am going to assume that this survive function will be called within a loop, such that i will be defined.
+  if(runif(1) <= survive[[x]]){ #just probability form the info$survive at location i
+    plant <- names(survive[x])
+    cell <- plant
   }else{
     cell <- ""
   }
 return(cell)
 }
+###########  END OF SRUVIVE FUNCTION ##############
+
+## Passing some dummy values to test the survive function as best I can; looks like it's working 
+# x <- 1
+# cell <- 0.2
+# survive(cell, info)
 
 
-# terrain is matrix I have called my.world
+##Will's skeleton of plant.timestep used terrain is his input matrix. My terrain is: my.world
+## But where do we define x, which is the plant species that we are looking at???
+## We call the survive function here, but no where do we define for which darned species!!
+
+
+#######  PLANT.TIMESTEP FUNCTION ############
 plant.timestep <- function(my.world, info){
   new.plants.matrix <- matrix(data = NA, nrow= nrow(my.world), ncol=ncol(my.world))
+  # Now we go through every cell in my.world to deterimine if we can put a plant in that cell location
+  # of our new matrix: new.plants.matrix
   for(i in 1:nrow(my.world)){
     for(j in 1:ncol(my.world)){
       newplant <- survive(my.world[i,j], info)
-      cat("newplant is:", newplant, "\n")
       new.plants.matrix[i,j] <- newplant
     }
   }
   return(new.plants.matrix)
 }
 
+##### END OF PLANT.TIMESTEP FUNCTION ###########
 
 
+# ## creating a dummy matrix for my.world
+# ## woould be nice to test to see if plant.timestep function works
+# data <- rnorm(10, 1, 20)
+# my.world <- matrix(data =data, nrow = 5, ncol = 5)
+# my.world
+# 
+# # now testing the new.plants.matrix -- something looks like it's happening/working? Shocking, indeed!
+# x <- 1
+# new.plants.matrix <- plant.timestep(my.world, info)
+# new.plants.matrix
 
-## creating a dummy matrix for my.world
-## woould be nice to test to see if function works
-data <- rnorm(10, 1, 20)
-my.world <- matrix(data =data, nrow = 5, ncol = 5)
-my.world
-new.plants.matrix <- plant.timestep(my.world, info)
-new.plants.matrix
 
-# plants <- array("", dim=c(dim(terrain),timesteps+1))
-# #...why timesteps+1, do you think?... Because you are entering info into the next timestep!
-# for(i in seq_len(dim(plants)[3]))
-#   plants[,,i][is.na(terrain)] <- NA
+######## PLANTS ARRAY #############
+plants <- array("", dim=c(dim(terrain),timesteps+1))
+#...why timesteps+1, do you think?... Because you are entering info into the next timestep!
+for(i in seq_len(dim(plants)[3]))
+  plants[,,i][is.na(terrain)] <- NA
 
+###### END OF PLANTS ARRAY ##########
